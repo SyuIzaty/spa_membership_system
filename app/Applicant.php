4d621a7af4c59d48e46420d6ec2413aca54aa47c
izzatwahid92 <izzat.wahid@intec.edu.my>
2020-07-27 16:45:37 +0800

    set fillable fields

--- app/Applicant.php
+++ app/Applicant.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Applicant extends Model
 {
     protected $table = 'applicant';
+    // Set mass-assignable fields
+    protected $fillable = ['applicant_name', 'applicant_ic', 'applicant_email', 'applicant_phone', 'applicant_nationality', 'applicant_programme','programme_name'];
 
     public function applicantresult()
     {
