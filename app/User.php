a0fd96ef1ca9cb2a2df6209e269aae96c84fee26
yuzizali <yuzi.zali@intec.edu.my>
2020-10-06 18:04:01 +0800

    remove hasPermission trait

--- app/User.php
+++ app/User.php
@@ -14,7 +14,7 @@ class User extends Authenticatable
 {
     protected $connection = 'auth';
     
-    use Notifiable, HasRoles, HasPermissions;
+    use Notifiable, HasRoles;
     
     /**
      * The attributes that are mass assignable.
