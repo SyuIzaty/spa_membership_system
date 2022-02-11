2f3149636be5777a1578421cc6ed90b5fb9923e4
zirahazir <nurhazirah0811@gmail.com>
2022-02-11 10:11:26 +0800

    Add Super Admin

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,10 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::before(function($user){
+            if ($user->hasRole('Super Admin')){
+                return true;
+            }
+        });
     }
 }
