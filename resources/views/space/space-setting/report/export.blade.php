1c007bd12bee9e41babb204a165f7f57d1e5952d
Nur Hazirah Mohd Azhar <hazirah.azhar@intec.edu.my>
2024-03-25 09:39:23 +0800

    Space - Add condition & remark in report

--- resources/views/space/space-setting/report/export.blade.php
+++ resources/views/space/space-setting/report/export.blade.php
@@ -1,96 +1,110 @@
 <head>
-  <meta charset="UTF-8">
-  <title>REPORT</title>
+    <meta charset="UTF-8">
+    <title>REPORT</title>
 </head>
 
 <body>
-  <table width="100%">
-    <tr>
-      <th style="background-color: #A8B6E3;border:1px solid black" rowspan="3">BLOCK</th>
-      <th style="background-color: #A8B6E3;border:1px solid black" rowspan="3">FLOOR</th>
-      <th style="background-color: #A8B6E3;border:1px solid black" rowspan="3">TYPE</th>
-      <th style="background-color: #A8B6E3;border:1px solid black" rowspan="3">NAME</th>
-      <th style="background-color: #A8B6E3;border:1px solid black" rowspan="3">STATUS</th>
-      <th style="background-color: #A8B6E3;border:1px solid black" colspan="{{ ($type->count() + $category->count()) * 3 }}">EQUIPMENT</th>
-    </tr>
-    <tr>
-      @foreach($type as $types)
-        <td colspan="3" style="border:1px solid black">{{ $types->asset_type }}</td>
-      @endforeach
-      @foreach($category as $categories)
-        <td colspan="3" style="border:1px solid black">{{ $categories->name }}</td>
-      @endforeach
-    </tr>
-    <?php
-      $total = $type->count() + $category->count();
-    ?>
-    <tr>
-      @for($i=1;$i<=$total;$i++)
-        <td style="border:1px solid black">Brand</td>
-        <td style="border:1px solid black">Serial No</td>
-        <td style="border:1px solid black">Quantity</td>
-      @endfor
-    </tr>
-    @foreach($room as $rooms)
-      <tr>
-        <td style="border:1px solid black">{{ $rooms->spaceBlock->name }}</td>
-        <td style="border:1px solid black">{{ $rooms->floor }}</td>
-        <td style="border:1px solid black">{{ $rooms->spaceRoomType->name }}</td>
-        <td style="border:1px solid black">{{ $rooms->name }}</td>
-        <td style="border:1px solid black">{{ $rooms->spaceStatus->name }}</td>
-        @foreach($type as $types)
-          <td style="border:1px solid black">
-            @foreach($rooms->spaceItems->where('item_category',1)->where('item_id',$types->id) as $it)
-            {{ $it->name ?? '-' }} 
-              @if($rooms->spaceItems->where('item_category',1)->where('item_id',$types->id)->count() >= 2)
-              <br>
-              @endif
+    <table width="100%">
+        <tr>
+            <th style="background-color: #A8B6E3;border:1px solid black" rowspan="3">BLOCK</th>
+            <th style="background-color: #A8B6E3;border:1px solid black" rowspan="3">FLOOR</th>
+            <th style="background-color: #A8B6E3;border:1px solid black" rowspan="3">TYPE</th>
+            <th style="background-color: #A8B6E3;border:1px solid black" rowspan="3">NAME</th>
+            <th style="background-color: #A8B6E3;border:1px solid black" rowspan="3">STATUS</th>
+            <th style="background-color: #A8B6E3;border:1px solid black"
+                colspan="{{ ($type->count() + $category->count()) * 3 }}">EQUIPMENT</th>
+            <th style="background-color: #A8B6E3;border:1px solid black" rowspan="3">CONDITION</th>
+            <th style="background-color: #A8B6E3;border:1px solid black" rowspan="3">REMARK</th>
+        </tr>
+        <tr>
+            @foreach ($type as $types)
+                <td colspan="3" style="border:1px solid black">{{ $types->asset_type }}</td>
             @endforeach
-          </td>
-          <td style="border:1px solid black">
-            @foreach($rooms->spaceItems->where('item_category',1)->where('item_id',$types->id) as $it)
-            {{ ($it->serial_no) ?? '-' }}
-              @if($rooms->spaceItems->where('item_category',1)->where('item_id',$types->id)->count() >= 2)
-                <br>
-              @endif
+            @foreach ($category as $categories)
+                <td colspan="3" style="border:1px solid black">{{ $categories->name }}</td>
             @endforeach
-          </td>
-          <td style="border:1px solid black">
-            @foreach($rooms->spaceItems->where('item_category',1)->where('item_id',$types->id) as $it)
-            {{ $it->quantity ?? '-' }}
-            @if($rooms->spaceItems->where('item_category',1)->where('item_id',$types->id)->count() >= 2)
-              <br>
-            @endif
-            @endforeach
-          </td>
+        </tr>
+        <?php
+        $total = $type->count() + $category->count();
+        ?>
+        <tr>
+            @for ($i = 1; $i <= $total; $i++)
+                <td style="border:1px solid black">Brand</td>
+                <td style="border:1px solid black">Serial No</td>
+                <td style="border:1px solid black">Quantity</td>
+            @endfor
+        </tr>
+        @foreach ($room as $rooms)
+            <tr>
+                <td style="border:1px solid black">{{ $rooms->spaceBlock->name }}</td>
+                <td style="border:1px solid black">{{ $rooms->floor }}</td>
+                <td style="border:1px solid black">{{ $rooms->spaceRoomType->name }}</td>
+                <td style="border:1px solid black">{{ $rooms->name }}</td>
+                <td style="border:1px solid black">{{ $rooms->spaceStatus->name }}</td>
+                @foreach ($type as $types)
+                    <td style="border:1px solid black">
+                        @foreach ($rooms->spaceItems->where('item_category', 1)->where('item_id', $types->id) as $it)
+                            {{ $it->name ?? '-' }}
+                            @if ($rooms->spaceItems->where('item_category', 1)->where('item_id', $types->id)->count() >= 2)
+                                <br>
+                            @endif
+                        @endforeach
+                    </td>
+                    <td style="border:1px solid black">
+                        @foreach ($rooms->spaceItems->where('item_category', 1)->where('item_id', $types->id) as $it)
+                            {{ $it->serial_no ?? '-' }}
+                            @if ($rooms->spaceItems->where('item_category', 1)->where('item_id', $types->id)->count() >= 2)
+                                <br>
+                            @endif
+                        @endforeach
+                    </td>
+                    <td style="border:1px solid black">
+                        @foreach ($rooms->spaceItems->where('item_category', 1)->where('item_id', $types->id) as $it)
+                            {{ $it->quantity ?? '-' }}
+                            @if ($rooms->spaceItems->where('item_category', 1)->where('item_id', $types->id)->count() >= 2)
+                                <br>
+                            @endif
+                        @endforeach
+                    </td>
+                @endforeach
+                @foreach ($category as $categories)
+                    <td style="border:1px solid black">
+                        @foreach ($rooms->spaceItems->where('item_category', 3)->where('item_id', $categories->id) as $it)
+                            {{ $it->name ?? '-' }}
+                            @if ($rooms->spaceItems->where('item_category', 3)->where('item_id', $categories->id)->count() >= 2)
+                                <br>
+                            @endif
+                        @endforeach
+                    </td>
+                    <td style="border:1px solid black">
+                        @foreach ($rooms->spaceItems->where('item_category', 3)->where('item_id', $categories->id) as $it)
+                            {{ $it->serial_no ?? '-' }}
+                            @if ($rooms->spaceItems->where('item_category', 3)->where('item_id', $categories->id)->count() >= 2)
+                                <br>
+                            @endif
+                        @endforeach
+                    </td>
+                    <td style="border:1px solid black">
+                        @foreach ($rooms->spaceItems->where('item_category', 3)->where('item_id', $categories->id) as $it)
+                            {{ $it->quantity ?? '-' }}
+                            @if ($rooms->spaceItems->where('item_category', 3)->where('item_id', $categories->id)->count() >= 2)
+                                <br>
+                            @endif
+                        @endforeach
+                    </td>
+                @endforeach
+                <td style="border:1px solid black">
+                    @foreach ($rooms->spaceRoomConditions as $c)
+                        {{ $c->spaceCondition->name ?? '-' }} - {{ $c->spaceCondition->description ?? '-' }}
+                        @if ($rooms->spaceRoomConditions->count() > 0)
+                            <br>
+                        @endif
+                    @endforeach
+                </td>
+                <td style="border:1px solid black">
+                    {{ $rooms->remark ?? '-' }}
+                </td>
+            </tr>
         @endforeach
-        @foreach($category as $categories)
-          <td style="border:1px solid black">
-            @foreach($rooms->spaceItems->where('item_category',3)->where('item_id',$categories->id) as $it)
-            {{ $it->name ?? '-' }} 
-              @if($rooms->spaceItems->where('item_category',3)->where('item_id',$categories->id)->count() >= 2)
-              <br>
-              @endif
-            @endforeach
-          </td>
-          <td style="border:1px solid black">
-            @foreach($rooms->spaceItems->where('item_category',3)->where('item_id',$categories->id) as $it)
-            {{ ($it->serial_no) ?? '-' }}
-              @if($rooms->spaceItems->where('item_category',3)->where('item_id',$categories->id)->count() >= 2)
-                <br>
-              @endif
-            @endforeach
-          </td>
-          <td style="border:1px solid black">
-            @foreach($rooms->spaceItems->where('item_category',3)->where('item_id',$categories->id) as $it)
-            {{ $it->quantity ?? '-' }}
-            @if($rooms->spaceItems->where('item_category',3)->where('item_id',$categories->id)->count() >= 2)
-              <br>
-            @endif
-            @endforeach
-          </td>
-        @endforeach
-      </tr>
-    @endforeach
-  </table>
+    </table>
 </body>
